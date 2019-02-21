19a3373fd2d81998de38f04dc7ab65ca2912abcd
adilsondias2018 <adilsondias@outlook.com>
2019-02-21 12:17:08 -0300

    Simple create Form| Primeira tela de Cadasto

--- SalesWebMvc01/Controllers/SellersController.cs
+++ SalesWebMvc01/Controllers/SellersController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc01.Models;
 using SalesWebMvc01.Services;
 
 namespace SalesWebMvc01.Controllers
 {
     public class SellersController : Controller
     {
-        // declarando uma dependencia para o SellerService
-
+        // declarando uma dependencia para o SellerService, sempre terei que coloar a dependência tanto no controller com no service.
         private readonly SellerService _sellerService;
 
+        // criando o construtor no controler para receber os dados 
         public SellersController (SellerService sellerService )
         {
             _sellerService = sellerService;
@@ -20,9 +21,28 @@ namespace SalesWebMvc01.Controllers
 
         public IActionResult Index()
         {
-
+            // chamada do FindAll()
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        // Criando um Nova ação
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Informando que a ação não de GET e sim de POST
+        [HttpPost] // anotacion POST
+        [ValidateAntiForgeryToken] // Anotacion  Impdede ataques SRFS que se aproveitar da sessanão de autenticaão para fazer coisas indesejadas.
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
     }
 }
--- SalesWebMvc01/Services/SellerService.cs
+++ SalesWebMvc01/Services/SellerService.cs
@@ -8,9 +8,10 @@ namespace SalesWebMvc01.Services
 {
     public class SellerService
     {
-
+        // Declarando um dependênca para a classe Service o readonly faz com que a dependência não possa ser alterada.
         private readonly SalesWebMvc01Context _context;
 
+        // Criando um constrututor para injeção de dependência possa ocorrer.
         public SellerService(SalesWebMvc01Context context)
         {
             _context = context;
@@ -21,7 +22,24 @@ namespace SalesWebMvc01.Services
         // buscando o dados no banco e exibindo em uma lista.
         public List<Seller> FindAll()
         {
+
+            /* Esse seria o select de todos os vendedores no bando
+             * perceba que os dados são acessando no contex do seller e a função ToList() lista os dados dessa "tabela seller"
+             * 
+             * Detalhe essa é um operação Sincorna que deixa a consulta ainda lenta o melhor seria trabalhar com um 
+             * operação assincrona.
+             */
             return _context.Seller.ToList();
+
+
+        }
+
+        public void Insert(Seller obj)
+        {   
+            // criando o objeto 
+            _context.Add(obj);
+            // gravando as mudanças no banco
+            _context.SaveChanges();
         }
 
 
